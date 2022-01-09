334a279528899c1b02cd39c757c206b6c5e049f5
Gabriel J. Osorio H <g.osorioe90@gmail.com>
2022-01-09 17:37:28 -0500

    Update settings.py

--- first_project/settings.py
+++ first_project/settings.py
@@ -22,7 +22,6 @@ print(STATIC_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-enatv-ww3tb6@1=#d=$cr%)i$1!%-lf*@c$@pc%w)uu*3pcd_n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
